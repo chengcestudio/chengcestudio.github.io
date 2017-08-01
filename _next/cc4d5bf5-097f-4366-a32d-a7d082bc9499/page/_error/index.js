77bdc13b5bd0b1b2e9a1dabba0508a02573ec851
carllx <carllx@hotmail.com>
2017-08-01 14:57:22 +0200

    加入/mui(materiale design UI),CNAME

--- _next/cc4d5bf5-097f-4366-a32d-a7d082bc9499/page/_error/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([6],{388:function(e,t,r){e.exports=r(389)},389:function(e,t,r){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=r(16),o=u(n),s=r(7),a=u(s),l=r(8),d=u(l),f=r(17),i=u(f),c=r(18),p=u(c),_=r(6),v=u(_),h=function(e){function t(){return(0,a.default)(this,t),(0,i.default)(this,(t.__proto__||(0,o.default)(t)).apply(this,arguments))}return(0,p.default)(t,e),(0,d.default)(t,[{key:"render",value:function(){return v.default.createElement("p",null,this.props.statusCode?"An error "+this.props.statusCode+" occurred on server":"An error occurred on client")}}],[{key:"getInitialProps",value:function(e){var t=e.res,r=e.jsonPageRes;return{statusCode:t?t.statusCode:r?r.status:null}}}]),t}(v.default.Component);t.default=h}},[388]);
-            return { page: comp.default }
-          })
-        
