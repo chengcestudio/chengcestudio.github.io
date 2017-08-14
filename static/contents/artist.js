22e378cf4845d223fbe82fdf5dce6a3bf3a9eca5
carllx <carllx@hotmail.com>
2017-08-14 16:05:07 +0200

    放弃material模版, 测试微信

---
+++ static/contents/artist.js
@@ -0,0 +1,25 @@
+export default [
+  {
+    "artista": 'Enzo Cucchi.jpg',
+    "id": 1,
+    'avatar':'EnzoCucchi',
+    "title": "piu' young artista di braccianti",
+    "description": "Enzo Cucchi nasce a Morro D’Alba nel 1949 da una famiglia di braccianti.\nAdolescente si sposta in Ancona, dove si avvicina all’arte da autodidatta, lavorando come assistente presso restauratori di libri e quadri. La sua passione per la poesia lo porta alla casa editrice Nuova Foglio di Macerata, dove conosce Gino de Dominicis, Pio Monti e Achille Bonito Oliva.\nSi sposta a Roma ed entra in contatto con Sandro Chia e Francesco Clemente. Viene teorizzata la Transavanguardia (che confluisce nel post-modernismo). La produzione di Cucchi è segnata dal ritorno al metodo e agli elementi classici dell’opera d’arte: quadri, disegni, grafiche, sculture in bronzo e ceramica.\nL’arte di Cucchi è prevalentemente figurativa e da molti definita visionaria."
+  },{
+    "artista": 'PangMaokun.jpg',
+    "id": 2,
+    'avatar':'Enzo Cucchi',
+    "title": "Batman of chiese",
+    "description": "Pang Maokun è nato a Chongqing nel 1963. Dal 1978 al 1981 ha frequentato il Liceo affiliato all’Accademia di Belle Arti del Sichuan; dal 1981 al 1988 ha inoltre studiato presso il Dipartimento di Pittura ad Olio dell’Accademia di Belle Arti del Sichuan, conseguendo nel 1988 un Master. Attualmente è Rettore dell’Accademia di Belle Arti del Sichuan, Presidente dell’Associazione degli Artisti di Chongqing, Rettore dell’Accademia di Pittura di Chongqing, Direttore della Galleria d’Arte di Chongqing, Membro del Comitato Consultivo dell’Associazione degli Artisti Cinesi, Vice-Direttore del Comitato per la Pittura ad Olio dell’Associazione degli Artisti Cinesi, Membro del Comitato Consultivo dell’Istituto di Pittura ad Olio Cinese.\nPer diversi anni ha perseguito nell’intento di esplorare il linguaggio della pittura ad olio classica nelle espressioni e rappresentazioni del contesto culturale contemporaneo, accostando le sue personali creazioni artistiche alla realtà sociale. Delineando sempre la realtà percepita dal suo sguardo, ha anche conferito all’originario stile classico permeato di solenne serenità una sorta di distanza ed estraneità propria del quotidiano. Un tale temperamento artistico non solo rappresenta la creazione e la rifinitura di uno stile personale maturo ma ancor più svela il valore e il significato della pittura ad olio nel contesto dell’arte contemporanea. Sia in patria che all’estero, le sue opere suono state più volte esibite e collezionate da istituzioni artistiche così come da privati. Tra le opere rappresentative possono essere annoverate «Le mele sono mature», «L’arcobaleno silenziosamente nel cielo», «Estate dorata», «La serie indistinta», «La serie del tempo virtuale», «La serie della coincidenza», «La serie scintillante», «La serie visione del mondo fluttuante» e così via."
+  },{
+    "artista": 'omniseligendi.gif',
+    "id": 3,
+    'avatar':'Enzo Cucchi',
+    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    "description": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+  }
+]
+
+
+
+
