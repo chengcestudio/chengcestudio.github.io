801b5e655cd284d26aebeb268ea5252ae3d8bc07
carllx <carllx@hotmail.com>
2017-08-19 04:13:17 +0200

    增加progres xhr

--- _next/3060db12-f31e-4fc9-b5f7-bb5a742fae26/page/_error/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([7],{381:function(e,t,r){e.exports=r(382)},382:function(e,t,r){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=r(9),o=u(n),s=r(6),a=u(s),l=r(7),d=u(l),f=r(10),i=u(f),c=r(11),p=u(c),_=r(4),v=u(_),h=function(e){function t(){return(0,a.default)(this,t),(0,i.default)(this,(t.__proto__||(0,o.default)(t)).apply(this,arguments))}return(0,p.default)(t,e),(0,d.default)(t,[{key:"render",value:function(){return v.default.createElement("p",null,this.props.statusCode?"An error "+this.props.statusCode+" occurred on server":"An error occurred on client")}}],[{key:"getInitialProps",value:function(e){var t=e.res,r=e.jsonPageRes;return{statusCode:t?t.statusCode:r?r.status:null}}}]),t}(v.default.Component);t.default=h}},[381]);
-            return { page: comp.default }
-          })
-        
