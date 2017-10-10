ddd51ef4f91efb7aecac8d7b2807d02103f3dbfa
carllx <carllx@hotmail.com>
2017-10-10 23:03:21 +0200

    artist 完善desktop端排版, 尝试修复section隐藏后仍然,自带长度造成一直下滚问题

--- _next/8d29945f-dc90-4dca-99fb-4bd0de19346f/page/_template/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/_template', function() {
-            var comp = module.exports=webpackJsonp([12],{426:function(e,t,u){e.exports=u(427)},427:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=u(4),a=l(n),r=u(1),f=l(r),o=u(3),d=l(o),c=u(5),i=l(c),s=u(6),p=l(s),_=u(0),m=l(_),h=u(69),v=l(h),E=function(e){function t(e){return(0,f.default)(this,t),(0,i.default)(this,(t.__proto__||(0,a.default)(t)).call(this,e))}return(0,p.default)(t,e),(0,d.default)(t,[{key:"render",value:function(){return m.default.createElement("main",null,m.default.createElement(v.default,null,m.default.createElement("title",null,"Template")),"<Test/>")}}]),t}(_.Component);t.default=E}},[426]);
-            return { page: comp.default }
-          })
-        
