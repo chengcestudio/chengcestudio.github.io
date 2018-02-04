ca4167508ad1bb125254c66903f37c1848a16efa
chengcestudio <chengcestudio@gmail.com>
2018-02-04 19:06:04 +0100

    4.根据 arunoda/arunoda.me 设置的路由方式,export静态网页

--- _next/eb4c60c8-7414-4133-b23f-50c2920175a7/page/news/indexs.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/news/indexs', function() {
-            var comp = module.exports=webpackJsonp([14],{581:function(t,e,n){t.exports=n(582)},582:function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var o=n(0),s=function(t){return t&&t.__esModule?t:{default:t}}(o);e.default=function(){return s.default.createElement("div",{id:"post-list"},"posts.map(post => (\n              <PostLink\n                key={post.slug}\n                title={post.title}\n                date={post.date}\n              />\n            ))")}}},[581]);
-            return { page: comp.default }
-          })
-        
--- static/contents/news/index.js
+++ static/contents/news/index.js
@@ -1,34 +1,11 @@
-/**
- * 给 < next.config > 生成路由文件
- * 给 NAV 传送扼要信息
- *
- * 艺术家页面json模板,艺术家总目录
- * 详情每个艺术家分别建立文件夹
- * @type {object}
- *
- * artista -  艺术家头像图片
- * id - 数组 index
- * name - 姓名
- * description - 描述
- *
- */
-
-const artistplan2018 = require("./2018_artists_plan.js");
-
-
-const NEWS =  [artistplan2018]
-
-
-// 发送到 nav NEWS 使用
-module.exports.newsInfo = NEWS.map(
-
-        (item)=>{
-            return {
-                id:item.id,
-                title:item.title,
-                // avatar:item.avatar,??? 图片
-                data:item.data,
-            }
-        }
-    )
-
+export default [
+    {
+        "title": {
+            "it": "2018 L'artista cinese ha intenzione di rimanere a Firenze",
+            "zh": "2018 中国艺术家赴意大利佛罗伦萨驻留计划",
+            "en": "2018 Chinese artists plan to stay in Florence, Italy"
+          },
+          slug: '2018_artists_plan',
+          date: '2018-2-4'
+    }
+]
