7105401f7030e6dd86e1eca84720f8e8abf11959
carllx <carllx@hotmail.com>
2018-02-05 15:05:24 +0100

    根据上传的附件申请表格更换下载地址

--- static/contents/news/index.js
+++ static/contents/news/index.js
@@ -2,7 +2,7 @@ export default [
     {
         "title": {
             "it": "2018 L'artista cinese ha intenzione di rimanere a Firenze",
-            "zh": "2018 中国艺术家赴意大利佛罗伦萨驻留计划",
+            "zh": "2018中国青年艺术家赴意大利佛罗伦萨驻留计划",
             "en": "2018 Chinese artists plan to stay in Florence, Italy"
           },
           slug: '2018_artists_plan',
