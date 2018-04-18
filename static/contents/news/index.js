7bd750b49e08304458370055060c867df91d590f
carllx <carllx@hotmail.com>
2018-04-18 10:28:58 +0200

    News增加2018交换佛美项目

--- static/contents/news/index.js
+++ static/contents/news/index.js
@@ -1,6 +1,15 @@
 export default [
     {
         "title": {
+            "it": "Bando 2018 per Scambio estivo con ACCADEMIA DELLE BELLE ARTI DI FIRENZE",
+            "zh": "2018中国四川美术学院 赴意大利佛罗伦萨美术学院暑期交换生项目",
+            "en": "2018 Florence Academy of Fine Arts Summer Exchange Program"
+          },
+          slug: '2018_accademia',
+          date: '2018-4-17'
+    },
+    {
+        "title": {
             "it": "Bando 2018 per progetto Residenze Artistiche",
             "zh": "2018中国青年艺术家赴意大利佛罗伦萨驻留计划",
             "en": "Artist In Residence Program"
